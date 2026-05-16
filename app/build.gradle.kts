5e0d19dbfcebc79ca244d0e76d0a9907d6232f07
范敬博 <231880036@smail.nju.edu.cn>
2026-05-16 17:39:17 +0800

    chore: add ucrop dependency and manifest entry

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.github.yalantis:ucrop:2.2.10-native")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
